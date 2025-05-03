f9da8dad48f71bb6c4ab6408900549c480b6d808
jhonatan alves <jhonatanalves1@gmail.com>
2025-05-03 20:00:45 -0300

    criação das rotas /posts e /posts/:id

--- server.js
+++ server.js
@@ -1,6 +1,27 @@
 import express from "express";
 
+const posts = [
+    { id: 1, descricao: "Neo um gato muito sério", imagem: "https://placecats.com/neo_2/500/500" },
+    { id: 2, descricao: "Millie & Neo dois gatos preguiçosos", imagem: "https://placecats.com/millie_neo/500/500" },
+    { id: 3, descricao: "Millie um gato manhoso", imagem: "https://placecats.com/millie/500/500" },
+];
+
+function buscarPostPorID(id) {
+    const post = posts.find(p => p.id === Number(id));
+    return post;
+}
+
 const app = express();
+app.use(express.json());
+
 app.listen(3000, () => {
     console.log("Servidor escutando...");
+});
+
+app.get("/posts", (req, res) => {
+    res.status(200).json(posts);
+});
+
+app.get("/posts/:id", (req, res) => {
+    res.status(200).json(buscarPostPorID(req.params.id));
 });
